bba4e160de457ae908ff9b304415f6bed021edc6
Hugo <104032491+hugoglvs@users.noreply.github.com>
2024-05-29 18:17:21 +0200

    Camera added

--- FaceGen/home/urls.py
+++ FaceGen/home/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     # /home/
     path('', views.index, name='index'),
-    path('generate/', views.generate, name='generate')
-    # /home/card/
-    # path('card/', views.card, name='card')
+    path('generate/', views.generate, name='generate'),
+    path('upload_photos/', views.upload_photos, name='upload_photos'),
 ]
--- FaceGen/home/views.py
+++ FaceGen/home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import ImageOutput, ImageInput
 from django.conf import settings
 import os
+import json
+import base64
 
 flag = True
 
@@ -40,6 +42,17 @@ def generate(request: HttpRequest) -> HttpResponse:
     context = {"image_output": image_output}
     return render(request, 'home/generate.html', context)
 
+@csrf_exempt
+def upload_photos(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        photos = data.get('photos', [])
+        for idx, photo in enumerate(photos):
+            photo_data = base64.b64decode(photo.split(',')[1])
+            with open(f'media/photo_{idx}.png', 'wb') as f:
+                f.write(photo_data)
+        return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'failed'})
 
 # def generated_image(request: HttpRequest) -> HttpResponse:
 
