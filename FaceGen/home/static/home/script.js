031ca37314fb947327ed9896ed69c125f91d8444
Hugo <104032491+hugoglvs@users.noreply.github.com>
2024-05-30 11:40:16 +0200

    Added icons

--- FaceGen/home/static/home/script.js
+++ FaceGen/home/static/home/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-    const $historiqueItems = $('.historique-item');
+    historyHandler();
+    cameraHandler();
+});
 
-    $historiqueItems.each(function() {
+function historyHandler() {
+    const $historyItems = $('.historique-item');
+
+    $historyItems.each(function() {
         const $item = $(this);
         const $image = $item.find('img');
         const $description = $item.find('p');
@@ -24,4 +29,56 @@ $(document).ready(function() {
             });
         });
     });
-});
+}
+function cameraHandler() {
+    $('#camera').on('click', function() {
+        navigator.mediaDevices.getUserMedia({ video: true })  // -> Promise<MediaTrackConstraints>
+            // The promise is resolved with a MediaStream object if the user grants permission
+            .then((stream) => {
+                const video = $('#webcam')[0];
+                video.srcObject = stream;
+                video.play();
+            })
+
+            .catch((err) => {
+                console.error("Error accessing the webcam: " + err);
+            });
+
+    
+        const canvas = $('#canvas')[0];
+        const video = $('#webcam')[0];
+        const context = canvas.getContext('2d');
+        const photos = [];
+        let photoCount = 0;
+
+        const takePhoto = () => {
+            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+            photos.push(canvas.toDataURL('image/png'));
+            photoCount++;
+
+            if (photoCount < 5) {
+                setTimeout(takePhoto, 1000); // Take a photo every second
+            } else {
+                // Upload photos to the server
+                $.ajax({
+                    url: '/home/upload_photos/',
+                    method: 'POST',
+                    contentType: 'application/json',
+                    headers: {
+                        'X-CSRFToken': '{{ csrf_token }}'
+                    },
+                    data: JSON.stringify({ photos: photos }),
+                    success: function(response) {
+                        console.log(response);
+                        // Redirect to generate.html or perform other actions
+                        
+                    },
+                    error: function(error) {
+                        console.error('Error:', error);
+                    }
+                });
+            }
+        };
+        takePhoto();
+    });
+}
